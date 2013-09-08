3c35dce18446c8e02b7859897231f45ce4be09df
Gawain Bracy II <g2musicgroup@fullsail.edu>
2013-09-07 17:50:23 -0700

    Added project file
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

---
+++ testApp/testApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Gawain Bracy II on 9/7/13.
+//  Copyright (c) 2013 Gawain Bracy II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testApp/testApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Gawain Bracy II on 9/7/13.
+//  Copyright (c) 2013 Gawain Bracy II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
