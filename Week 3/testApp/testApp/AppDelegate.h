6799fb95b2cc30a653cfc10b5dc6351c2d49a70f
Gawain Bracy II <g2musicgroup@fullsail.edu>
2013-09-19 20:27:25 -0700

    Added new Week 3 Project
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

---
+++ Week 3/testApp/testApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Gawain Bracy II on 9/19/13.
+//  Copyright (c) 2013 Gawain Bracy II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
